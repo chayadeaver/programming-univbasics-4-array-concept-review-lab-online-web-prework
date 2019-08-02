2d0964eb688b365768c3cf25c6d6162fca06a14b
Chaya Deaver <chayadeaver@gmail.com>
2019-08-02 15:02:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,8 +9,10 @@ array.length.times do |index|
  end
 
 def find_max_value(array)
+  max_value = 0 
   array.length.times do |index|
-    
+    if array[index] > max_value
+  end
 end
 
 def find_min_value(array)
